825dab0706918829c63906358a79e1e9abeda0eb
Alexander Yaroslavtsev <a1exweb@mail.ru>
2021-01-28 07:42:48 +0300

    lesson-1

---
+++ lesson-1/script.js
@@ -0,0 +1,9 @@
+alert('Hello!');
+let money = 10000000,
+    income = 1000000,
+    addExpenses = 300000,
+    deposit = 90000000,
+    mission = 1000000000,
+    period = 30;
+const patronymic = 'Vasilivich';
+console.log(`My patronymic is ${patronymic}.`);
