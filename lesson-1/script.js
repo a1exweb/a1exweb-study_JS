53f0dc2f93af3c0a7237132bb604cc64bea41252
Alexander Yaroslavtsev <a1exweb@mail.ru>
2021-01-28 10:33:32 +0300

    format

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -1,4 +1,3 @@
-alert('Hello!');
 let money = 10000000,
     income = 1000000,
     addExpenses = 300000,
@@ -6,4 +5,6 @@ let money = 10000000,
     mission = 1000000000,
     period = 30;
 const patronymic = 'Vasilivich';
+
+alert('Hello!');
 console.log(`My patronymic is ${patronymic}.`);
