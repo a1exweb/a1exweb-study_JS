f936d159bb16304f09101370c85c7411a1bce7da
Alexander Yaroslavtsev <a1exweb@mail.ru>
2021-02-24 12:05:00 +0300

    fix

--- script/script.js
+++ script/script.js
@@ -62,7 +62,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
         document.addEventListener('click', (e) => {
             const target = e.target;
-            if(target === closeBtn || target.closest('ul')) {
+            if(target === closeBtn || target.closest('.active-menu ul')) {
                 handlerMenu();
             } else if (target.closest('.menu')) {
                 handlerMenu();
